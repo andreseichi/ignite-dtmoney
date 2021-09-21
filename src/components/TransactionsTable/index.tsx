9446603fe4ba5c7fb5a70118b6c7c0e312034d70
André Tashiro <andre_seichi@hotmail.com>
2021-09-21 17:02:45 -0300

    componente TransactionsTable

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,41 @@
+import { Container } from './styles';
+
+export function TransactionsTable() {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Desenvolvimento site</td>
+            <td className="deposit">R$12.000</td>
+            <td>Desenvolvimento</td>
+            <td>21/09/2021</td>
+          </tr>
+
+          <tr>
+            <td>Alguem</td>
+            <td className="withdraw">- R$1.100</td>
+            <td>Casa</td>
+            <td>21/09/2021</td>
+          </tr>
+
+          <tr>
+            <td>Salário</td>
+            <td className="deposit">R$3.000</td>
+            <td>Desenvolvimento</td>
+            <td>21/09/2021</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
+}
